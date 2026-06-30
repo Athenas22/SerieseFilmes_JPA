06c04a6651fa798e9aeaa86c754cb336e176cf48
Luana Gabriella <luana.gabriella@academico.ifpb.edu.br>
2026-06-30 15:52:55 -0300

    Update Genero.java

--- src/main/java/modelo/Genero.java
+++ src/main/java/modelo/Genero.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType; // Importação adicionada
+import jakarta.persistence.FetchType; 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +21,6 @@ public class Genero {
     
     private String nome;
 
-    // CORREÇÃO: Adicionado o fetch = FetchType.LAZY
     @ManyToMany(mappedBy = "generos", fetch = FetchType.LAZY)
     private List<Serie> series = new ArrayList<>();
 
