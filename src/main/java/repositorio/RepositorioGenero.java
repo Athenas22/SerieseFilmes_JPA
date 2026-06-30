c7f71f243329bcd659d1de30e9271fd9c45b6de9
Luana Gabriella <luana.gabriella@academico.ifpb.edu.br>
2026-06-30 15:56:08 -0300

    Update RepositorioGenero.java

--- src/main/java/repositorio/RepositorioGenero.java
+++ src/main/java/repositorio/RepositorioGenero.java
@@ -8,13 +8,11 @@ public class RepositorioGenero extends Repositorio<Genero> {
 
     @Override
     public Genero localizar(Object chave) {
-        // Encontra um género pelo ID (chave primária)
         return Util.getManager().find(Genero.class, chave);
     }
 
     @Override
     public List<Genero> listar() {
-        // Lista todos os géneros cadastrados
         return Util.getManager().createQuery("select g from Genero g", Genero.class).getResultList();
     }
 }
